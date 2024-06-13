4a4eb2a0765f544494bc8e8665a62ab631d85a18
rushikesh1410 <rushi14102812@gmail.com>
2024-06-13 12:17:18 +0530

    commit

---
+++ src/test/java/com/product/Key_Cloak_SpringBoot1/KeyCloakSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.Key_Cloak_SpringBoot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCloakSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
